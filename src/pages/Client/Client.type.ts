121aa53d90e34e4980aab89f8ceea9d6e2147ecc
Max <gironmaxence.pro@gmail.com>
2022-04-10 22:25:31 +0200

    Delete src directory

--- src/pages/Client/Client.type.ts
+++
@@ -1,9 +0,0 @@
-export default interface ClientData {
-    id?: any,
-    codeClient: string,
-    lastName: string,
-    firstName: string,
-    birthDate: string,
-    address: string,
-    createdAt: string
-}
--- src/pages/Folder/Folder.type.ts
+++
@@ -1,8 +0,0 @@
-export default interface FolderData {
-    id?: any,
-    code: string,
-    description: string,
-    startDate: string,
-    status: boolean,
-    endDate: string
-}
